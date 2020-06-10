36f59c9f64ac3beea13454aabdc31503ab5c5f56
sara altaee <sara.korea96@gmail.com>
2020-06-10 23:15:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+class breakfast{
+  constructor(food,drink){
+    this.food=food;
+    this.drink=drink;
+  }
+}
+
+class lunch{
+  constructor(salad,soup,drink){
+    this.salad=salad;
+    this.soup=soup;
+    this.drink=drink;
+  }
+}
+
+class dinner{
+  constructor(salad ,soup,entree,dessert){
+    this.salad=salad;
+    this.soup=soup;
+    this.entree=entree;
+    this._dessert=dessert;
+  }
+}
+
+let breakfastfood=new breakfast("bancake","milk");
+breakfastfood;
